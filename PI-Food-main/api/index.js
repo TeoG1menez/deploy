a57c7f5c884349c5a669ace66220ec1c4fd407d0
TeoG1menez <teo.unr.gimenez@gmail.com>
2023-01-18 20:08:30 -0300

    s

--- PI-Food-main/api/index.js
+++ PI-Food-main/api/index.js
@@ -1,6 +1,7 @@
 
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+require('dotenv').config();
 const port = process.env.PORT || 3001
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
